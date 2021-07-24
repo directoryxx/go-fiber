b2d7aa053e2e930cc952ac9088576014710f3205
directoryx <anggawijaya4567@gmail.com>
2021-07-24 21:37:57 +0700

    try to add gorm mysql and try to use handler route

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	db, err := gorm.Open(mysql.Open("root:password@(mysql-server)/dev"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(&db)
+
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
@@ -21,9 +32,15 @@ func main() {
 		Level: compress.LevelBestSpeed,
 	}))
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, World!")
+	// })
+
+	app.Get("/", home)
 
 	log.Fatal(app.Listen(":8010"))
+}
+
+func home(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
 }
